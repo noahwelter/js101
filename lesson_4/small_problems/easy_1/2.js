029732fddb3fc5ef909983a4fcc46f7290587f8d
Noah Welter <noahwelter@gmail.com>
2021-06-19 11:56:12 -0500

    Complete Easy 2 - 2

---
+++ lesson_4/small_problems/easy_1/2.js
@@ -0,0 +1,17 @@
+// Original solution
+for (let num = 1; num < 100; num += 2) {
+  console.log(num);
+}
+
+// Alternate solution with user input
+function printOdd(startNum, endNum) {
+  if (startNum % 2 === 0) startNum += 1;
+  let num = startNum;
+
+  while (num <= endNum) {
+    console.log(num);
+    num += 2;
+  }
+}
+
+printOdd(2, 35);
