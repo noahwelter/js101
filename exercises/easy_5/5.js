36f4ee1c67be152ccaac23118f58be8ba2756d0a
Noah Welter <noahwelter@gmail.com>
2021-06-28 11:07:11 -0500

    Complete Easy 5 - 5

---
+++ exercises/easy_5/5.js
@@ -0,0 +1,24 @@
+// Using push
+function interleave(arr1, arr2) {
+  let combined = [];
+  for (let index = 0; index < arr1.length; index += 1) {
+    combined.push(arr1[index], arr2[index]);
+  }
+
+  return console.log(combined);
+}
+
+// Using forEach
+// function interleave(arr1, arr2) {
+//   let combined = [];
+//   combined.length = arr1.length + arr2.length;
+
+//   arr1.forEach((value, index) => {
+//     combined[index * 2] = value;
+//     combined[(index * 2) + 1] = arr2[index];
+//   });
+
+//   return console.log(combined);
+// }
+
+interleave([1, 2, 3], ['a', 'b', 'c']);    // [1, "a", 2, "b", 3, "c"]
