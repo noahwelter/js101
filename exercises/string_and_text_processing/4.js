48cab115eda638ac62156ac90f8b214d678e0dfe
Noah Welter <noahwelter@gmail.com>
2021-07-06 17:53:15 -0500

    Complete String and Text Processing - 4

---
+++ exercises/string_and_text_processing/4.js
@@ -0,0 +1,9 @@
+let wordCap = str => {
+  return str.split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+};
+
+wordCap('four score and seven');       // "Four Score And Seven"
+wordCap('the javaScript language');    // "The Javascript Language"
+wordCap('this is a "quoted" word');    // 'This Is A "quoted" Word'
