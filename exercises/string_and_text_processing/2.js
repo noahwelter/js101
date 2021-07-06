fded0347fde7c02fedce8ba4df43bb94bf421c95
Noah Welter <noahwelter@gmail.com>
2021-07-06 17:26:07 -0500

    Complete String and Text Processing - 2

---
+++ exercises/string_and_text_processing/2.js
@@ -0,0 +1,7 @@
+let removeVowels = arr => {
+  return arr.map(str => str.replace(/[aeiou]/gi, ''));
+};
+
+removeVowels(['abcdefghijklmnopqrstuvwxyz']);         // ["bcdfghjklmnpqrstvwxyz"]
+removeVowels(['green', 'YELLOW', 'black', 'white']);  // ["grn", "YLLW", "blck", "wht"]
+removeVowels(['ABC', 'AEIOU', 'XYZ']);                // ["BC", "", "XYZ"]
