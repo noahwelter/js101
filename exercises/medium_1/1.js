9f9ea61a40460e22ee32ed996a708f426d992e2d
Noah Welter <noahwelter@gmail.com>
2021-07-06 23:02:58 -0500

    Complete Medium 1 - 1

---
+++ exercises/medium_1/1.js
@@ -0,0 +1,26 @@
+let rotateArray = arr => {
+  if (!Array.isArray(arr)) return undefined;
+
+  let copy = arr.slice();
+
+  if (copy.length !== 0) copy.push(copy.shift());
+
+  return copy;
+};
+
+rotateArray([7, 3, 5, 2, 9, 1]);       // [3, 5, 2, 9, 1, 7]
+rotateArray(['a', 'b', 'c']);          // ["b", "c", "a"]
+rotateArray(['a']);                    // ["a"]
+rotateArray([1, 'a', 3, 'c']);         // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]);    // [[1, 2], 3, { a: 2 }]
+rotateArray([]);                       // []
+
+// return `undefined` if the argument is not an array
+rotateArray();                         // undefined
+rotateArray(1);                        // undefined
+
+
+// the input array is not mutated
+let array = [1, 2, 3, 4];
+rotateArray(array);                    // [2, 3, 4, 1]
+console.log(array);                                 // [1, 2, 3, 4]
