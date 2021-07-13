c07b54ebb6da0eeef2604028ffe6348155f1158c
Noah Welter <noahwelter@gmail.com>
2021-07-13 17:24:01 -0500

    Print hollow diamond

--- exercises/interpretive_problem_solving/2.js
+++ exercises/interpretive_problem_solving/2.js
@@ -27,21 +27,48 @@
 - Code:
 */
 
-let diamond = num => {
-  let center = (num - 1) / 2;
-  let topArr = [];
-  let centerArr = ['*'.repeat(num)];
+// Filed diamond
+// let diamond = num => {
+//   let center = (num - 1) / 2;
+//   let topArr = [];
+//   let centerArr = ['*'.repeat(num)];
 
-  for (let row = 0; row < center; row += 1) {
-    let spaces = ' '.padStart(center - row);
-    let stars = '*'.repeat((row * 2) + 1);
-    topArr.push(spaces + stars);
+//   for (let row = 0; row < center; row += 1) {
+//     let spaces = ' '.padStart(center - row);
+//     let stars = '*'.repeat((row * 2) + 1);
+//     topArr.push(spaces + stars);
+//   }
+
+//   topArr
+//     .concat(centerArr, topArr.slice().reverse())
+//     .forEach(row => console.log(row));
+// };
+
+// Hollow diamond
+function diamond(num) {
+  numberSequence(num).forEach(number => {
+    let str = ' '.repeat((num - number) / 2) +
+      '*' +
+      ' '.repeat(Math.floor(number / 2));
+    console.log(str + str.split('').reverse().join('').slice(1));
+  });
+}
+
+function numberSequence(num) {
+  let result = [];
+  let increment = 2;
+  let number = 1;
+
+  while (number > 0) {
+    result.push(number);
+    if (number === num) {
+      increment = -2;
+    }
+    number += increment;
   }
 
-  topArr
-    .concat(centerArr, topArr.slice().reverse())
-    .forEach(row => console.log(row));
-};
+  return result;
+}
 
 diamond(1);
 diamond(3);
